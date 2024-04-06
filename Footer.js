6a542caac328a34b89669501aaf91ec7a53419a6
2100030306Nav <110444840+2100030306Nav@users.noreply.github.com>
2024-04-06 12:02:42 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>Exam details</p>
+      <p>Madala Navadeep 2100030306</p>
+    </div>
+  );
+}
+
+export default Footer;
